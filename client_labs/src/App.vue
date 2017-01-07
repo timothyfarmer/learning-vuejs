765fb79370176d1c20501d5f1939e6f97d413984
Tim F <tfarmer4@gmail.com>
2017-01-07 16:02:34 -0600

    handling nav bar being hidden when user not logged in. refactored a little as well to use components better

--- client_labs/src/App.vue
+++ client_labs/src/App.vue
@@ -1,54 +1,30 @@
+<script>
+  import {mapState} from 'vuex'
+  import TopMenu from './components/TopMenu'
+  export default {
+    components: {
+      TopMenu
+    },
+    created () {
+      console.log('I have been created')
+      const userObj = JSON.parse(window.localStorage.getItem('authUser'))
+      this.$store.dispatch('setUserObject', userObj)
+    },
+    computed: {
+      ...mapState({
+        userStore: state => state.userStore
+      })
+    }
+  }
+</script>
+
 <template>
-  <div class="wrapper" id="app">
-    <section class="login">
-      <div class="row">
-        <div class="col-md-6 col-md-push-3">
-          <div class="panel panel-default">
-            <div class="panel-heading"><strong>Login</strong></div>
-            <div class="panel-body">
-              <form v-on:submit.prevent="handleLoginFormSubmit()">
-                <div class="form-group">
-                  <label>Email Address</label>
-                  <input 
-                    type="text" 
-                    class="form-control"
-                    v-model="login.email" 
-                    placeholder="Enter Your Email" 
-                  >
-                </div>
-                <div class="form-group">
-                  <label>Password</label>
-                  <input 
-                    type="password"
-                    class="form-control"
-                    v-model='login.password'
-                    placeholder="Enter Your Password" 
-                  >
-                </div>
-                <button class="btn btn-primary">Login</button>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+  <div>
+    <pre>{{ userStore }}</pre>
+    <top-menu></top-menu>
+    <router-view></router-view>
   </div>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      login: {
-        email: '',
-        password: ''
-      }
-    }
-  },
-  components: {
-  }
-}
-</script>
 <style lang="sass">
   @import '../node_modules/bootstrap/dist/css/bootstrap.css'
   @import '../node_modules/bootstrap/dist/css/bootstrap-theme.css'
--- client_labs/src/main.js
+++ client_labs/src/main.js
@@ -1,15 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
 import Vue from 'vue'
-import App from './App'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
+import store from './store'
+
+import App from './App'
+
+import DashboardPage from './pages/DashboardPage'
+import LoginPage from './pages/LoginPage'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
 
+Vue.component('app', App)
+
 const routes = [
-  {path: '/', component: App, name: 'home'}
+  {path: '/', component: LoginPage, name: 'home'},
+  {path: '/dashboard', component: DashboardPage, name: 'dashboard', meta: { 'requiresAuth': true }}
 ]
 
 const router = new VueRouter({
@@ -17,6 +26,18 @@ const router = new VueRouter({
   routes // short for routes: routes
 })
 
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAuth) {
+    const authUser = JSON.parse(window.localStorage.getItem('authUser'))
+    if (authUser && authUser.access_token) {
+      next()
+    } else {
+      next({name: 'home'})
+    }
+  }
+  next()
+})
+
 new Vue({
-  router
+  router, store
 }).$mount('#app')
